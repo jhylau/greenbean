a295d7de238108eecdd37281677f9d765161264f
Jonathan Lau <jhylau1@gmail.com>
2014-03-26 22:44:43 -0400

    fixed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,17 +9,7 @@ Event.delete_all
 Restaurant.delete_all
 Signup.delete_all
 LunchGroup.delete_all
-first_event = Event.create(:event_date => Date.new(2014,4,4))
+first_event = Event.create(:event_date => Date.new(2014,4,11))
 quicpic = Restaurant.create(:name =>"Quic Pic")
 deguru = Restaurant.create(:name =>"Deguru")
 kingston = Restaurant.create(:name =>"Kingston Station")
-
-Signup.create(:email => 1, :event_id => first_event.id)
-Signup.create(:email => 2, :event_id => first_event.id)
-Signup.create(:email => 3, :event_id => first_event.id)
-Signup.create(:email => 4, :event_id => first_event.id)
-Signup.create(:email => 5, :event_id => first_event.id)
-Signup.create(:email => 6, :event_id => first_event.id)
-Signup.create(:email => 7, :event_id => first_event.id)
-Signup.create(:email => 8, :event_id => first_event.id)
-Signup.create(:email => 9, :event_id => first_event.id)
